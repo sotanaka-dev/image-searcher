3d4611942c407cd7c69de91197299cf67d114dbb
sotanaka <sotanaka.dev@gmail.com>
2023-08-13 12:21:57 +0900

    feat: LPを作成

---
+++ frontend/react-app/src/pages/Landing.js
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import styles from "../styles/pages/Landing.module.scss";
+
+export default function Landing() {
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.infoGroup}>
+        <img
+          src={`${process.env.PUBLIC_URL}/header_logo.png`}
+          alt=""
+          className={styles.logo}
+        />
+        <p className={styles.overview}>
+          普段から画像・動画系のSNSをよく利用する人のための、それらのSNSの検索結果を1ヶ所で確認できるサービスです。
+        </p>
+        <div className={styles.btnWrap}>
+          <Link to="/users" className={styles.btn}>
+            サインアップ
+          </Link>
+          <Link to="/users/signin" className={styles.btn}>
+            サインイン
+          </Link>
+        </div>
+      </div>
+
+      <div className={styles.imgWrap}>
+        <img
+          src={`${process.env.PUBLIC_URL}/sample.png`}
+          alt=""
+          className={styles.sampleImg}
+        />
+      </div>
+    </div>
+  );
+}
